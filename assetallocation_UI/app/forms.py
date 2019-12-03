6e8d2f9e8dd81911685d1a5e77693712f5897bcd
Jérémie <Anais.Jeremie@lgim.com>
2019-12-03 11:59:30 +0000

    AA UI Flask

---
+++ assetallocation_UI/app/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Length
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message='Passwords must be 6 charachters or greater.')])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class ChangePassword(FlaskForm):
+    old_password = PasswordField('Old Password', [DataRequired()])
+    new_password = PasswordField('New Password', [DataRequired(), EqualTo('confirm', message='Passwords must match'),Length(min=6, message='Passwords must be 6 charachters or greater.')])
+    confirm = PasswordField('Repeat Password')
+
+
+
+
+
+class AssetAllocationDetails(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, message='Passwords must be 6 charachters or greater.')])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
