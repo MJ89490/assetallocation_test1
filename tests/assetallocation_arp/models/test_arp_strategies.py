26ec480c822d02943f480fac7728604ab4d71659
Jérémie <Anais.Jeremie@lgim.com>
2019-12-11 09:44:42 +0000

    Modifications Code and Add Unit-Testing

---
+++ tests/assetallocation_arp/models/test_arp_strategies.py
@@ -0,0 +1,13 @@
+"""
+Created on 12/11/2019
+Author: AJ89720
+email: Anais.Jeremie@lgim.com
+"""
+
+import pytest
+
+
+
+def test_get_inputs_from_excel():
+    pass
+
---
+++ tests/assetallocation_arp/models/test_times.py
@@ -0,0 +1,6 @@
+"""
+Created on 12/11/2019
+Author: AJ89720
+email: Anais.Jeremie@lgim.com
+"""
+
