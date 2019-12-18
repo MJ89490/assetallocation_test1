3273c0a12462faef737b2dff490e6a356a8f7183
Jérémie <Anais.Jeremie@lgim.com>
2019-12-18 17:01:26 +0000

    Unit-Test: Add Returns R Positioning Results

--- tests/assetallocation_arp/data.py
+++ tests/assetallocation_arp/data.py
@@ -22,15 +22,28 @@ class Data(object):
         self.signals, self.returns, self.r, self.positioning = times.format_data_and_calc(self.times_inputs, self.asset_inputs, self.all_data)
         return self.signals, self.returns, self.r, self.positioning
 
-class Test(Data):
-    def __init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning):
-        Data.__init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning)
-
-    def test_format_data_and_calc(self):
-        Test.get_data(self)
-        signals, returns, r, positioning = Test.get_times_data(self)
-
-        return signals, returns, r, positioning
+# class Test(Data):
+#     def __init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning):
+#         Data.__init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning)
+#
+#     def test_format_data_and_calc(self):
+#         Test.get_data(self)
+#         signals, returns, r, positioning = Test.get_times_data(self)
+#
+#         return signals, returns, r, positioning
+# class UnitTestt(Data):
+#     def __init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning):
+#         Data.__init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning)
+#
+#     def test_format_data_and_calc(self):
+#         UnitTestt.get_data(self)
+#         signals, returns, r, positioning = UnitTestt.get_times_data(self)
+#
+#         expected_signals = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "signals_v_to_test"))
+#         d_expected = pd.read_csv(expected_signals, index_col=0, sep='\t')
+#         pd.testing.assert_frame_equal(returns.reset_index(drop=True), d_expected.reset_index(drop=True),
+#                                       check_column_type=False)
+#         #return signals, returns, r, positioning
 
 if __name__ == "__main__":
     times_input = pd.DataFrame
@@ -42,4 +55,4 @@ if __name__ == "__main__":
     positioning = pd.DataFrame
 
     obj2 = Test(times_input, asset_inputs, all_data, signals, returns, r, positioning)
-    a,b,c,d = obj2.test_format_data_and_calc()
+    obj2.test_format_data_and_calc()
--- tests/assetallocation_arp/models/test_times.py
+++ tests/assetallocation_arp/models/test_times.py
@@ -6,27 +6,13 @@ email: Anais.Jeremie@lgim.com
 
 import pytest
 import os
+import mock
 from assetallocation_arp.models import times
 from assetallocation_arp.data_etl import import_data as gd
 import pandas as pd
 CURRENT_PATH = os.path.dirname(__file__)
 
-# @pytest.mark.parametrize("original_results, expected_results",
-#                          [(os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_for_test", "signals")),
-#                            os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "signals_v_to_test"))),
-#                           (os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_for_test", "returns")),
-#                            os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "returns_v_to_test"))),
-#                           (os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_for_test", "r")),
-#                            os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "r_v_to_test"))),
-#                           (os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_for_test", "positioning")),
-#                            os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "positioning_v_to_test")))])
-#
-# def test_format_data_and_calc(original_results, expected_results):
-#     d_original = pd.read_csv(original_results, index_col=0, sep='\t')
-#     d_expected = pd.read_csv(expected_results,  index_col=0, sep='\t')
-#     pd.testing.assert_frame_equal(d_original, d_expected, check_names=False, check_column_type=False)
-
-class Data(object):
+class Data:
     def __init__(self, times_inputs, asset_inputs, all_data, signals, returns, r, positioning):
         self.leverage = "v" # to modify in excel
         self.times_inputs = times_inputs
@@ -42,58 +28,28 @@ class Data(object):
 
     def get_times_data(self):
         self.signals, self.returns, self.r, self.positioning = times.format_data_and_calc(self.times_inputs, self.asset_inputs, self.all_data)
-        return self.signals
+        return self.signals, self.returns, self.r, self.positioning
 
-class Test(Data):
-    def __init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning):
-        Data.__init__(self, times_input, asset_inputs, all_data, signals, returns, r, positioning)
+def test_format_data_and_calc():
 
-    def test_format_data_and_calc(self):
-        Test.get_data(self)
-        signals, returns, r, positioning = Test.get_times_data(self)
+        data_object = Data(times_inputs=pd.DataFrame, asset_inputs=pd.DataFrame, all_data=pd.DataFrame, signals=pd.DataFrame, returns=pd.DataFrame, r=pd.DataFrame, positioning=pd.DataFrame)
+        data_object.get_data()
+        signals_origin, returns_origin, r_origin, positioning_origin = data_object.get_times_data()
 
         expected_signals = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "signals_v_to_test"))
-        d_expected = pd.read_csv(expected_signals, index_col=0, sep='\t')
-        pd.testing.assert_frame_equal(signals.reset_index(drop=True), d_expected.reset_index(drop=True),
-                                      check_column_type=False)
-        #return signals, returns, r, positioning
+        expected_returns = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "returns_v_to_test"))
+        expected_r = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "r_v_to_test"))
+        expected_positioning = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "positioning_v_to_test"))
 
-if __name__ == "__main__":
-    times_input = pd.DataFrame
-    asset_inputs = pd.DataFrame
-    all_data = pd.DataFrame
-    signals = pd.DataFrame
-    returns = pd.DataFrame
-    r = pd.DataFrame
-    positioning = pd.DataFrame
+        dataframe_signals = pd.read_csv(expected_signals, index_col=0, sep='\t')
+        dataframe_returns = pd.read_csv(expected_returns, index_col=0, sep='\t')
+        dataframe_r = pd.read_csv(expected_r, index_col=0, sep='\t')
+        dataframe_positioning = pd.read_csv(expected_positioning, index_col=0, sep='\t')
 
-    obj2 = Test(times_input, asset_inputs, all_data, signals, returns, r, positioning)
-    obj2.test_format_data_and_calc()
+        pd.testing.assert_frame_equal(signals_origin.reset_index(drop=True), dataframe_signals.reset_index(drop=True), check_column_type=False)
+        pd.testing.assert_frame_equal(returns_origin.reset_index(drop=True), dataframe_returns.reset_index(drop=True), check_column_type=False)
+        pd.testing.assert_frame_equal(r_origin.reset_index(drop=True), dataframe_r.reset_index(drop=True), check_column_type=False)
+        pd.testing.assert_frame_equal(positioning_origin.reset_index(drop=True), dataframe_positioning.reset_index(drop=True), check_column_type=False)
 
 
 
-
-
-# @pytest.mark.parametrize("to_test, test",
-#                          [("signals_to_test", Data.signals)])
-# def test_format_data_and_calc(to_test, test):
-#     #change the name of the leverage "v"
-#     times_inputs, asset_inputs, all_data = gd.extract_inputs_and_mat_data("times", None, None)
-#     signals, returns, r, positioning = times.format_data_and_calc(times_inputs, asset_inputs, all_data)
-#
-#     print(test)
-#     expected_signals = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "signals_v_to_test"))
-#     d_expected = pd.read_csv(expected_signals, index_col=0, sep='\t')
-#     pd.testing.assert_frame_equal(signals.reset_index(drop=True), d_expected.reset_index(drop=True), check_column_type=False)
-#
-#     expected_returns = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "returns_v_to_test"))
-#     d_expected = pd.read_csv(expected_returns, index_col=0, sep='\t')
-#     pd.testing.assert_frame_equal(returns.reset_index(drop=True), d_expected.reset_index(drop=True), check_column_type=False)
-#
-#     expected_returns = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "r_v_to_test"))
-#     d_expected = pd.read_csv(expected_returns, index_col=0, sep='\t')
-#     pd.testing.assert_frame_equal(r.reset_index(drop=True), d_expected.reset_index(drop=True), check_column_type=False)
-#
-#     expected_returns = os.path.abspath(os.path.join(CURRENT_PATH, "..", "data_times_to_test", "positioning_v_to_test"))
-#     d_expected = pd.read_csv(expected_returns, index_col=0, sep='\t')
-#     pd.testing.assert_frame_equal(positioning.reset_index(drop=True), d_expected.reset_index(drop=True), check_column_type=False)
