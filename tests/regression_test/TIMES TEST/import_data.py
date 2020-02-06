4b50108d42a1f4e4ca75a60fedc3060505d2c806
Anais Jeremie <anais.jeremie@lgim.com>
2020-02-06 15:29:53 +0000

    Add resources directory

---
+++ tests/regression_test/TIMES TEST/import_data.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import ARP as arp
+
+def dataimport_future (file):
+    data=pd.read_excel(file+".xlsx",sheet_name="Data", index_col=[0], header=3,skiprows=[4,5,6,7])
+    data.index = pd.to_datetime(data.index, format='%d.%m.%Y %H:%M:%S')
+    data["SterlingEUR"]=(1+data["Sterling"])/(1+data["Euro"])-1
+    data["SkronaEUR"]=(1+data["Skrona"])/(1+data["Euro"])-1
+    data["NokronaEUR"]=(1+data["Nokrona"])/(1+data["Euro"])-1
+    data["SwissFrancEUR"]=(1+data["SwissFranc"])/(1+data["Euro"])-1 
+    return data
+
+def dataimport_index (file):
+    data=pd.read_excel(file+".xlsx",sheet_name="Data1", index_col=None, header=1,skiprows=[2,3,4],usecols=[0]+list(range(1,60,4)))
+    data2=pd.read_excel(file+".xlsx",sheet_name="Data2", index_col=None, header=1,skiprows=[2,3,4],usecols=list(range(0,64,4)))
+    data3=pd.read_excel(file+".xlsx",sheet_name="Data3", index_col=None, header=1,skiprows=[2,3,4],usecols=list(range(0,60,4)))
+    data=pd.concat([data, data2, data3], axis=1)
+    data.index = pd.to_datetime(data['Index'], format='%Y-%m-%d')
+    data=data.drop('Index',axis=1)
+    data["SterlingEUR"]=data["Sterling"]/data["Euro"]
+    data["SkronaEUR"]=data["Skrona"]/1+data["Euro"]
+    data["NokronaEUR"]=data["Nokrona"]/data["Euro"]
+    data["SwissFrancEUR"]=data["SwissFranc"]/data["Euro"]
+    return data
+
+def dataimport_CARRY (file):
+    data=pd.read_excel(file+".xlsx",sheet_name="Data1", index_col=None, header=1,skiprows=[2,3,4],usecols=[0]+list(range(3,60,4)))
+    data2=pd.read_excel(file+".xlsx",sheet_name="Data2", index_col=None, header=1,skiprows=[2,3,4],usecols=list(range(2,64,4)))
+    data3=pd.read_excel(file+".xlsx",sheet_name="Data3", index_col=None, header=1,skiprows=[2,3,4],usecols=list(range(2,60,4)))
+    data=pd.concat([data, data2, data3], axis=1)
+    data.index  = pd.to_datetime(data['Index'], format='%Y-%m-%d')
+    data=data.drop('Index',axis=1)
+    data.columns=[x[:-2] for x in data.columns]
+    return data
+
+def dataimport_VALUE (file):
+    data=pd.read_excel(file+".xlsx",sheet_name="Data1", index_col=None, header=1,skiprows=[2,3,4],usecols=[0]+list(range(4,60,4)))
+    data2=pd.read_excel(file+".xlsx",sheet_name="Data2", index_col=None, header=1,skiprows=[2,3,4],usecols=list(range(3,64,4)))
+    data3=pd.read_excel(file+".xlsx",sheet_name="Data3", index_col=None, header=1,skiprows=[2,3,4],usecols=list(range(3,60,4)))
+    data=pd.concat([data, data2, data3], axis=1)
+    data.index = pd.to_datetime(data['Index'], format='%Y-%m-%d')
+    data=data.drop('Index',axis=1)
+    data.columns=[x[:-2] for x in data.columns]
+    return data
+
+def dataimport_FICarry (file,market):
+    data=pd.read_excel(file+".xlsx",sheet_name=market, index_col=None, header=12,skiprows=[13])
+    data.index = pd.to_datetime(data['Index'], format='%Y-%m-%d')
+    data=data.drop('Index',axis=1)
+    return data
+
+def dataimport_FR (file):
+    data=pd.read_excel(file+".xlsx",sheet_name="Sheet1", index_col=[0], header=0)
+    data.index = pd.to_datetime(data.index, format='%d.%m.%Y %H:%M:%S')
+    return data
+
+def datacheck (future,file,diff):
+    # Check missing data and repeated numbers for last 20 days
+    print("Missing data")
+    print("**********************************")
+    test=future.iloc[-15:]
+    for column in test:
+        if len(test[test[column].isnull()][column])>0:
+                print(test[test[column].isnull()][column])
+    print("")
+    print("Stale data") 
+    print("**********************************")
+    test=future-future.diff(periods=1)[-15:]
+    for column in test:
+        if len(test[(test[column]==0)][column])>0:
+            print(test[(test[column]==0)][column])
+    print("")    
+    
+    # Check for outliers (>2 stdev)
+    print("Outliers")
+    print("**********************************")
+    if diff:
+        test=test.diff(periods=1)
+    else:
+        test=future
+    test=test.iloc[-15:]/test.std(axis=0)
+    test=test[((test<-2) | (test>2))]
+    for column in test:
+        if len(test[test[column].notnull()][column])>0:
+            print(test[test[column].notnull()][column])
+    print("")
+    
+    # Check for data revisions (>0.5stdev compared to historic data)
+    print("Data revisions")
+    print("**********************************")
+    test=pd.read_pickle(file+".pkl")
+    test=(test-future)/future.std(axis=0)
+    test=test[((test<-0.2) | (test>0.2))]
+    for column in test:
+        if len(test[test[column].notnull()][column])>0:
+            print(test[test[column].notnull()][column])
+
+
+future=dataimport_future("Future data")
+datacheck(future,"Future data",False)
+accept_data = input('Accept updated data? (y/n): ')
+if accept_data=='y':
+    future.to_pickle("Future data.pkl")
+    print('Future data imported')
+else:
+    print('Future data NOT imported')
+
+index=dataimport_index("Data")
+datacheck(index,"Data",True)
+accept_data = input('Accept updated data? (y/n): ')
+if accept_data=='y':
+    index.to_pickle("Data.pkl")
+    print('Index data imported')
+else:
+    print('Index data NOT imported')
+    
+settings=arp.dataimport_settings("Settings")
+#settings.to_pickle("Settings.pkl")
+
---
+++ tests/regression_test/TIMES TEST/times.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import math
+from pandas.tseries.offsets import BDay
+
+import ARP as arp
+
+# Parameters
+TIMES_LAG = 2
+settings = arp.dataimport_settings("Settings")
+
+# Change the universe of markets that is being used
+markets="Leverage_MATR"  # All "Leverage_all_markets" / Minimalist "Leverage_min"
+# Leverage/scaling of individual markets
+sleverage ="v"           #Equal(e) / Normative(n) / Volatility(v) / Standalone(s)
+
+def signal (index):
+    sig = pd.DataFrame()
+    for column in index:
+        sig1 = index[column].ewm(alpha=2/15).mean()/index[column].ewm(alpha=2/30).mean()-1
+        sig2 = index[column].ewm(alpha=2/30).mean()/index[column].ewm(alpha=2/60).mean()-1
+        sig3 = index[column].ewm(alpha=2/60).mean()/index[column].ewm(alpha=2/120).mean()-1
+        sig[column] = (sig1/sig1.rolling(window=90).std()+sig2/sig2.rolling(window=90).std()+sig3/sig3.rolling(window=90).std())/3
+        sig[column] = sig[column]*np.exp(-1*sig[column].pow(2)/6)/(math.sqrt(3)*math.exp(-0.5))
+    sig = arp.discretise(sig, "weekly")
+    sig = sig.shift(TIMES_LAG, freq="D")
+    return sig
+
+
+# Import data
+future = pd.read_pickle("Future data.pkl")
+index = pd.read_pickle("Data.pkl")
+series = ["S&P 500",	"Euro Stoxx 50",	"Nikkei 225",	"Hang Seng",	"Treasury",	"Gilt",	"Bund",	"Cad10",	"Yen",	"Euro",	"Aus",	"CanDollar", "Sterling"]
+
+future = future[series]
+index = index[series]
+ 
+sig = signal(index)
+costs = settings.loc["Costs"]
+marketselect = settings.loc[markets]
+
+if sleverage == 'e' or sleverage == 's':
+    leverage = 0*future+1
+    leverage[marketselect.index[marketselect > 0]] = 1
+elif sleverage == 'n':
+    leverage = 0*future+marketselect
+elif sleverage == 'v':
+    leverage=1/future.ewm(alpha=1/150, min_periods=10).std()
+else:
+    raise Exception('Invalid entry')
+leverage[marketselect.index[marketselect.isnull()]] = np.nan
+leverage = leverage.shift(periods=TIMES_LAG, freq='D', axis=0).reindex(future.append(pd.DataFrame(index=future.iloc[[-1]].index+BDay(2))).index,method='pad')
+
+if sleverage == 's':
+    (ret, R, positioning) = arp.returnTS(sig, future, leverage, 0*costs, 0)
+    ret1 = ret
+    R1 = R
+    positioning1 = positioning
+else:
+    (ret, R, positioning) = arp.returnTS(sig, future, leverage, 0*costs, 1)
+    (ret1, R1, positioning1) = arp.rescale(ret, R, positioning, "Total", 0.01)
+
+    # import data to csv files
+    ret1.to_csv('ret1_old', encoding='utf-8', index=False)
+    R1.to_csv('R1_old', encoding='utf-8', index=False)
+    positioning1.to_csv('positioning1_old', encoding='utf-8', index=False)
+    sig.to_csv('signals_old', encoding='utf-8', index=False)
+
+
