cfb66f6da1ae5c2e063bba69cb2186bb52768524
Anais Jeremie <anais.jeremie@lgim.com>
2020-04-03 12:10:40 +0200

    Add a new html template for home page

--- assetallocation_UI/app/routes.py
+++ assetallocation_UI/app/routes.py
@@ -30,7 +30,7 @@ def before_request():
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='HomePage')
+    return render_template('home_new.html', title='HomePage')
 
 
 @app.route('/login', methods=['GET'])
