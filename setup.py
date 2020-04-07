69056224bc917e909edbc8599ae56c07928c685b
Manimegalai Jaganathan <Manimegalai.Jaganathan@lgim.com>
2020-04-07 14:41:31 +0100

    modify the version in git

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ setup(
     ],
 
     name="assetallocation_arp",
-    version="0.0.7",
+    version="0.0.8",
     #version_config={
     #  "version_format": "{tag}.dev{sha}",
-    #  "starting_version": "0.0.7"
+    #  "starting_version": "0.0.8"
     #},
     description="sist[3~[3~[3~[3~[3~",
     long_description=open('README.md').read(),
