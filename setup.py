4b70a5492ae23019475f69710ee3c5827fe0d6de
Manimegalai Jaganathan <Manimegalai.Jaganathan@lgim.com>
2020-04-17 18:40:11 +0100

    working productionized models

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ setup(
     ],
 
     name="assetallocation_arp",
-    version="0.0.17",
+    version="0.0.18",
     #version_config={
     #  "version_format": "{tag}.dev{sha}",
-    #  "starting_version": "0.0.17"
+    #  "starting_version": "0.0.18"
     #},
     description="sist[3~[3~[3~[3~[3~",
     long_description=open('README.md').read(),
