9772fe5020f8a19429cf40b000c96e5ab437caab
Manimegalai Jaganathan <Manimegalai.Jaganathan@lgim.com>
2020-04-17 10:53:56 +0100

    changing the installer to added to the repo

--- aa_installer/installer.ps1
+++ aa_installer/installer.ps1
@@ -3,5 +3,5 @@ write-host "copy the installer from shared folder !"
 write-host "Install AssetAllocation and its dependencies"
 $file_name = Get-ChildItem *.whl
 pip install $file_name
-Sleep 60
+Sleep 60                      
 write-host "Installed AssetAllocation successfully!"
---
+++ assetallocation_arp/__init__.py
@@ -0,0 +1,4 @@
+from .common_libraries import frequency_types, leverage_types, models_names
+from .data_etl import
+from .oauth2_auth import OAuth2
+from .oauth2_session import OAuth2Session, TokenUpdated
