7ccb99b2d25d794b08b2fea4c7e892a718714dbf
JL89005 <josh.logan@lgim.com>
2020-04-28 11:16:11 +0100

    updating with recent progress

--- assetallocation_arp/models/curp.py
+++ assetallocation_arp/models/curp.py
@@ -7,94 +7,124 @@ CURP
 @author: JL89005
 """
 import pandas as pd
+import math
 import numpy as np
 import xlwings as xw
 
 
 # my formatting is that variables are camel case, functions use _
 
-
-
-
-
 # Curp Carve out file \\inv\LGIM\FrontOffice\Bonds\Asset Allocation\Research&Strategy\ARP\CURP Carve Out.xlsm
-
-
+# CUMO file \\inv\LGIM\FrontOffice\Bonds\Asset Allocation\Research&Strategy\ARP\CUMO.xlsm
 
 def run_curp(curp_inputs, asset_inputs, all_data ):
     # this will be where the main code is run
+    all_data['USDUSD Curncy'] = 1
+    all_data['USDUSDCR Curncy'] = 1
+    # this below should be deleted when the data is made available in the mat file
+    all_data['SEKUSD Curncy'] = 1
+    all_data['CHFUSD Curncy'] = 1
+    all_data['NOKUSD Curncy'] = 1
+    all_data['CHFUSDCR Curncy'] = 1
+    all_data['SEKUSDCR Curncy'] = 1
+    all_data['CADUSDCR Curncy'] = 1
+    all_data['NOKUSDCR Curncy'] = 1
 
-    # get data test
-    currencyCrosses = create_crosses_two(asset_inputs['Currency'])
-    pppData = filter_data(all_data, asset_inputs['PPP Tickers'])
-    irData = filter_data(all_data, asset_inputs['IR Tickers'])
-    spotSparseData = filter_data(all_data, asset_inputs['Spot Tickers'])
-    carrySparseData = filter_data(all_data, asset_inputs['Carry Tickers'])
+    # create all FX crosses from the ones given
+    currencyCrosses = create_crosses(asset_inputs['Currency'])
+    firstCurrency = currencyCrosses.applymap(lambda x: x[0:3])
+    secondCurrency = currencyCrosses.applymap(lambda x: x[3:6])
+
+    # Spot Data
+    firstCurrencySpotTicker = firstCurrency.applymap(lambda x: x +'USD Curncy')
+    firstCurrencySpotTickerList = firstCurrencySpotTicker['cross'].tolist()
+    secondCurrencySpotTicker = secondCurrency.applymap(lambda x: x + 'USD Curncy')
+    secondCurrencySpotTickerList = secondCurrencySpotTicker['cross'].tolist()
+    firstCurrencySpotData = all_data[firstCurrencySpotTickerList]
+    secondCurrencySpotData = all_data[secondCurrencySpotTickerList]
+    # Next line might need to change: see https://pythontic.com/pandas/dataframe-binaryoperatorfunctions/div
+    firstCurrencySpotDataRatio = firstCurrencySpotData.pct_change+1
+    secondCurrencySpotDataRatio = secondCurrencySpotData.pct_change+1
+    SpotData = firstCurrencySpotDataRatio.div(secondCurrencySpotDataRatio)-1
+
+    # Carry Data
+    firstCurrencyCarryTicker = firstCurrency.applymap(lambda x: x +'USDCR Curncy')
+    firstCurrencyCarryTickerList = firstCurrencyCarryTicker['cross'].tolist()
+    secondCurrencyCarryTicker = secondCurrency.applymap(lambda x: x + 'USDCR Curncy')
+    secondCurrencyCarryTickerList = secondCurrencyCarryTicker['cross'].tolist()
+    firstCurrencyCarryData = all_data[firstCurrencyCarryTickerList]
+    secondCurrencyCarryData = all_data[secondCurrencyCarryTickerList]
+    # Next line might need to change: see https://pythontic.com/pandas/dataframe-binaryoperatorfunctions/div
+    firstCurrencyCarryDataRatio = firstCurrencyCarryData.pct_change+1
+    secondCurrencyCarryDataRatio = secondCurrencyCarryData.pct_change + 1
+    carryData = firstCurrencyCarryDataRatio.div(secondCurrencyCarryDataRatio)-1
+
+    # PPP Data
+    firstCurrencyPPPTicker = firstCurrency.applymap(lambda x:asset_inputs.loc[asset_inputs['Currency'] == x, 'PPP Tickers'].iloc[0])
+    firstCurrencyPPPTickerList = firstCurrencyPPPTicker['cross'].tolist()
+    secondCurrencyPPPTicker = secondCurrency.applymap(lambda x:asset_inputs.loc[asset_inputs['Currency'] == x, 'PPP Tickers'].iloc[0])
+    secondCurrencyPPPTickerList = secondCurrencyPPPTicker['cross'].tolist()
+    firstCurrencyPPPData = all_data[firstCurrencyPPPTickerList]
+    secondCurrencyPPPData = all_data[secondCurrencyPPPTickerList]
+    pppData = secondCurrencyPPPData.div(firstCurrencyPPPData)
+
+    # IR Data
+    firstCurrencyIRTicker = firstCurrency.applymap(lambda x:asset_inputs.loc[asset_inputs['Currency'] == x, 'IR Tickers'].iloc[0])
+    firstCurrencyIRTickerList = firstCurrencyIRTicker['cross'].tolist()
+    secondCurrencyIRTicker = secondCurrency.applymap(lambda x:asset_inputs.loc[asset_inputs['Currency'] == x, 'IR Tickers'].iloc[0])
+    secondCurrencyIRTickerList = secondCurrencyIRTicker['cross'].tolist()
+    firstCurrencyIRData = all_data[firstCurrencyIRTickerList]
+    secondCurrencyIRData = all_data[secondCurrencyIRTickerList]
+    irData = firstCurrencyIRData.sub(secondCurrencyIRData)
+
 
     # process data
 
-    asset_inputs.set_index('Currency')
+    # Define some variables from curp_inputs
+    volWindow = curp_inputs.loc[curp_inputs['Variable Name'] == 'Volatility Window','CUMO'].iloc[0]
+    historicalLevelAveraging =curp_inputs.loc[curp_inputs['Variable Name'] == 'Historical Level Averaging','CUMO'].iloc[0]
+    window = curp_inputs.loc[curp_inputs['Variable Name']=='Window','CUMO'].iloc[0]
+    signal = curp_inputs.loc[curp_inputs['Variable Name']=='Signal','CUMO'].iloc[0]
+    ret = curp_inputs.loc[curp_inputs['Variable Name'] == 'Returns', 'CUMO'].iloc[0]
 
-    # create all FX crosses from the ones given
-    # this needs to be smart in that it takes the first part of the cross as one matrix, the second part as the other and does A/B
+    # Define which type of data should be used for the signals. From the inputs
+    # signalData = ...
+    if signal == 1 or signal == 5 or signal == 6:
+        signalData = spotData
+    else:
+        signalData = carryData
 
-    mtxSpot=pd.DataFrame({currencyCrosses:[]})
-    for (columnName, columnData) in mtxSpot.iteritems():
-        # get base fx
-        baseFx = columnName[0:3]
-        priceFX = columnName[2:5]
-        # need to find the ticker to use for the base fx
-        baseTicker = asset_inputs.loc[asset_inputs['Currency'] == baseFx, 'Spot Tickers'].iloc[0]
-        priceTicker = asset_inputs.loc[asset_inputs['Currency'] == priceFX, 'Spot Ticker'].iloc[0]
-        # Get the data for each of these columns, then divide or subtract the two and put it back into the DF
+    # returnData = ...
+    if ret == 1:
+        returnData = spotData
+    else:
+        returnData = carryData
+
+    vol = sqrt(12)*pd.rolling_std(signalData,volWindow)
+    # could take the rolling average then use 'shift' on the column
+    sharpeAvgData = pd.rolling_mean(rebasedSignalData,2*historicalLevelAveraging)
+    # I think i need to shift by 'Window' - 'Historic Volatility Window'
+    sharpeAvgDataOffset = sharpeAvgData.shift(window-historicalLevelAveraging)
+
+    if signal == 4:
+        denominator = 1
+    else:
+        denominator = sqrt(12)*pd.rolling_std(signalData,window)
+
+    sharpe = (math.log(rebasedSignalData.div(sharpeAvgDataOffset)) ** (12 / window) - 1)/denominator
 
 
-
-    firstCurrency = [frst(x) for x in currencyCrosses.cross]
-    secondCurrency = [scnd(x) for x in currencyCrosses.cross]
-    matrixSpotA = pd.DataFrame({firstCurrency})
-    matrixSpotB = pd.DataFrame({secondCurrency})
-    matrixCarryA = pd.DataFrame({firstCurrency})
-    matrixCarryB = pd.DataFrame({secondCurrency})
-
-    for currency in asset_inputs['Currency']:
-        matrixSpotA[firstCurrency == currency] = spotSparseData[(currency + 'USD Curncy')]
-        matrixSpotB[secondCurrency == currency] = spotSparseData[(currency + 'USD Curncy')]
-        matrixCarryA[firstCurrency == currency] = carrySparseData[(currency + 'USDCR Curncy')]
-        matrixCarryB[secondCurrency == currency] = carrySparseData[(currency + 'USDCR Curncy')]
-    spotData = matrixSpotA/matrixSpotB #this wont work
-    carryData = matrixCarryA/matrixCarryB #this wont work
-
-    """""""""""
-    For this section i need to calculate IR differentials
-    To do this:
-    Build 2 empty DF
-    """""""""""
 
     pass
 
 def create_crosses(currencyList):
     # create data frame
     output = pd.DataFrame(columns=['cross'])
-    for i in list(range(1,(len(currencyList.index)-1))):
-        for j in list(range(i+1,len(currencyList.index))):
-            output = output.append({'cross': currencyList[i]+currencyList[j]}, ignore_index=True)
+    for i in list(range(1,(len(currencyList.index)))):
+        for j in list(range(i+1,len(currencyList.index)+1)):
+            output = output.append(dict(cross=currencyList[i] + currencyList[j]), ignore_index=True)
     x = output
     return x
-# this function has a small problem with the indexing not being correct in the for's
-
-
-def frst(inpt):
-    return inpt[0:3]
-
-def scnd(inpt):
-    return inpt[3:6]
-
-def filter_data(all_data, tickers):
-    # pass in the entire load of data and only take the columns needed
-    output = pd.DataFrame(all_data,columns = tickers)
-    return output
-
 
 if __name__ == "__main__":
     # test inputs
