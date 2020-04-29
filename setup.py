8e2eb6250761bf5b14cab796037719afd80aeea2
investments_build_vcs_np_svc <jenkins@jenkins-investments-build-tools.np.openshift.inv.adroot.lgim.com>
2020-04-29 06:19:28 +0000

    modify the version in git

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ setup(
     ],
 
     name="assetallocation_arp",
-    version="0.0.2",
+    version="0.0.3",
     #version_config={
     #  "version_format": "{tag}.dev{sha}",
-    #  "starting_version": "0.0.2"
+    #  "starting_version": "0.0.3"
     #},
     description="",
     long_description=open('README.md').read(),
