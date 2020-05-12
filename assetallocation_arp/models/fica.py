fbd450bab95fa50409c5387e374fba9b98e05fa8
WK68945 <willem.klijnstra@lgim.com>
2020-05-12 21:12:40 +0100

    progressing on FICA

--- assetallocation_arp/models/fica.py
+++ assetallocation_arp/models/fica.py
@@ -4,31 +4,40 @@ FICA
 @author: WK68945 & AB
 """
 
+import numpy as np
 import pandas as pd
+from scipy.interpolate import CubicSpline
 
-def format_data(fica_inputs, asset_inputs, all_data):
+def format_data_and_calc(fica_inputs, asset_inputs, all_data):
 
     curve = pd.DataFrame()
     carry = pd.DataFrame()
 
-    T = ['3m', '1y', '2y', '3y', '4y', '5y', '6y', '7y', '8y', '9y', '10y', '15y', '20y', '30y']
-    T_c = ['3m', '1y', '9y', '10y']
-    country = asset_inputs['country']
-    type = fica_inputs['curve'].item()
+    c_type = fica_inputs['curve'].item()
     date_from = fica_inputs['date_from'].item()
     date_to = fica_inputs['date_to'].item()
-    asset_inputs_t = asset_inputs.set_index('country').T
+    country = asset_inputs['country']
+    tenor = asset_inputs['tenor']
 
     all_data = all_data.loc[date_from:date_to]
     all_data = all_data.asfreq('BM')
 
-    curve = all_data[asset_inputs[type + '_ticker_' + T[0]]]
-    carry = all_data[asset_inputs['cr_swap_ticker_' + T_c[0]]]
+    asset_inputs_t = asset_inputs.set_index('country').T
 
-    for i in range(1, 14):
-        curve = pd.merge(curve, all_data[asset_inputs[type+'_ticker_'+T[i]]], right_index=True, left_index=True)
+    if c_type == 'sovereign':
+        start = 0
+    else:
+        start = 14
 
-    for i in range(1, 4):
-        carry = pd.merge(carry, all_data[asset_inputs['cr_swap_ticker_'+T_c[i]]], right_index=True, left_index=True)
+    curve = all_data[asset_inputs_t['AUD'][start:start+14]]
+    carry = all_data[asset_inputs_t['AUD'][28:32]]
+
+    for i in range(2, 11):
+        curve = pd.merge(curve, all_data[asset_inputs_t[country[i]][start:start+14]], right_index=True, left_index=True)
+
+    for i in range(2, 11):
+        carry = pd.merge(carry, all_data[asset_inputs_t[country[i]][28:32]], right_index=True, left_index=True)
+
+    pewe = np.arange(0, 30, 0.5).tolist()
 
     return curve, carry
