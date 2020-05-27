5e9c1b587ec5ed535c760e2b29f9f36de54d38c3
WK68945 <willem.klijnstra@lgim.com>
2020-05-27 14:38:34 +0100

    Merge branch 'feature/curp' of gogs-ose-btprod.inv.adroot.lgim.com:SN69248/assetallocation_arp into FICAFinally
    # Conflicts:
    #	assetallocation_arp/arp_dashboard.xlsm

--- assetallocation_arp/models/maven.py
+++ assetallocation_arp/models/maven.py
@@ -6,19 +6,23 @@ FICA
 
 import numpy as np
 import pandas as pd
-import data_etl.data_manipulation as dm
+#import data_etl.data_manipulation as dm
 
 
 def format_data(maven_inputs, asset_inputs, all_data):
     # creating dataframe with index data
     # reading inputs and shortening data
-    asset = asset_inputs['asset']
     date_from = maven_inputs['date_from'].item()
     date_to = maven_inputs['date_to'].item()
+    frequency = maven_inputs['frequency'].item()
     all_data = all_data.loc[date_from:date_to]
-    all_data = dm.set_data_frequency(all_data, maven_inputs['frequency'].item(), maven_inputs['week_day'].item())
     #
-    if maven_inputs['excess_totalreturn'].item() == 'excess':
+    if frequency == 'monthly':
+        all_data = all_data.asfreq('BM')
+    else:
+        all_data = all_data.asfreq('W-' + maven_inputs['week_day'].item())
+    #
+    if maven_inputs['er_tr'].item() == 'excess':
         maven_indices = all_data[asset_inputs['bbg_er_ticker']]
     else:
         maven_indices = all_data[asset_inputs['bbg_tr_ticker']]
