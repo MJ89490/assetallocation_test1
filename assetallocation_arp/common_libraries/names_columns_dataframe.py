0123d2356207632c252d0453ef74dbac5d806c12
Anais Jeremie <anais.jeremie@lgim.com>
2020-06-24 19:37:12 +0200

    Add a new class enum for the names of the dataframe columns

---
+++ assetallocation_arp/common_libraries/names_columns_dataframe.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class CurrencySpot(enum.Enum):
+    Inflation_Differential = 'Inflation_Differential_'
+    Inflation_Release = "Inflation_Release"
+    Carry = 'Carry_'
+    Trend = 'Trend_'
+    Combo = 'Combo_'
+    Return_Ex_Costs = 'Return_Ex_Costs_'
+    Return_Incl_Costs = 'Return_Incl_Costs_'
+    Spot_Ex_Costs = 'Spot_Ex_Costs_'
+    Spot_Incl_Costs = 'Spot_Incl_Costs_'
+
+
+
+
+
+
+
