1ee0c9bd106c49f9690a1190f3b2aeb635541600
JS89275 <jessica.smart@lgim.com>
2020-07-16 10:03:07 +0100

    Add tests for class_attributes_to_df

--- assetallocation_arp/data_etl/dal/dal.py
+++ assetallocation_arp/data_etl/dal/dal.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 
 def class_attributes_to_df(cls_instances: List[Any]):
+    if not all(isinstance(i, type(cls_instances[0])) for i in cls_instances):
+        raise TypeError('All cls_instances should be of the same type')
+
     cols = [k[1:] if k.startswith('_') else k for k in cls_instances[0].__dict__.keys()]
     return pd.DataFrame([[v for v in cls.__dict__.values()] for cls in cls_instances], columns=cols)
 
