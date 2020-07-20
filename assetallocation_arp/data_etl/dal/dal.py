ef155327e3911021aa1ae398c0fe61b20dbc8ba4
JS89275 <jessica.smart@lgim.com>
2020-07-20 09:52:04 +0100

    Add get_fund_strategy method to Db class

--- assetallocation_arp/data_etl/dal/dal.py
+++ assetallocation_arp/data_etl/dal/dal.py
@@ -2,9 +2,6 @@ from typing import List, Any
 
 from pandas import DataFrame
 
-from .db import Db
-from .user import User
-
 
 def class_attributes_to_df(cls_instances: List[Any]) -> DataFrame:
     if not all(isinstance(i, type(cls_instances[0])) for i in cls_instances):
@@ -12,23 +9,6 @@ def class_attributes_to_df(cls_instances: List[Any]) -> DataFrame:
 
     cols = [k[1:] if k.startswith('_') else k for k in cls_instances[0].__dict__.keys()]
     return DataFrame([[v for v in cls.__dict__.values()] for cls in cls_instances], columns=cols)
-
-
-# TODO test the below
-def get_user_by_email(db: Db, user_email: str) -> User:
-    row = db.get_row_where_equal('user', 'user', {'email': user_email})
-    return User(row.email, row.id, row.name)
-
-
-# TODO test the below
-def get_user_by_id(db: Db, user_id: str) -> User:
-    row = db.get_row_where_equal('user', 'user', {'id': user_id})
-    return User(row.email, row.id, row.name)
-
-
-def get_fund_strategy(fund_name, strategy_name, business_datetime, system_datetime):
-    """select * from fund_strategy where id = fund_strategy_id"""
-    pass
 
 
 def get_fund_strategy_asset_analytics(fund_name, strategy_name, business_datetime, system_datetime):
--- assetallocation_arp/data_etl/dal/db.py
+++ assetallocation_arp/data_etl/dal/db.py
@@ -1,14 +1,27 @@
 from typing import List, Any, Dict
+from datetime import datetime
 
 from sqlalchemy import create_engine
 
+from .proc import Proc
+from .user import User
+from .fund import Fund
+from .strategy import Strategy
+from .fundstrategy import FundStrategy
+from .currency import Currency
+
 
 class Db:
+    procs = Proc.__members__.keys()
+
     def __init__(self, conn_str: str):
         self.engine = create_engine(conn_str)
         self.dbapi_conn = self.engine.raw_connection()
 
-    def call_proc(self, proc_name, proc_params: List[Any]):
+    def call_proc(self, proc_name: str, proc_params: List[Any]) -> List[Any]:
+        if proc_name not in self.procs:
+            raise ValueError(f'The stored procedure "{proc_name}" is not defined for the class Db')
+
         try:
             cursor = self.dbapi_conn.cursor()
             cursor.callproc(proc_name, proc_params)
@@ -21,5 +34,31 @@ class Db:
 
         return results
 
+    def get_user_by_email(self, user_email: str) -> User:
+        row = self.get_row_where_equal('user', 'user', {'email': user_email})
+        return User(row.email, row.id, row.name)
+
     def get_row_where_equal(self, schema: str, table: str, column_values: Dict[str, Any]):
         pass
+
+    def get_user_by_id(self, user_id: str) -> User:
+        row = self.get_row_where_equal('user', 'user', {'id': user_id})
+        return User(row.email, row.id, row.name)
+
+    # TODO test the below
+    def get_fund(self, name: str) -> Fund:
+        fund_id, currency, currency_id = self.call_proc(Proc.select_fund.name, [name])
+        return Fund(Currency(currency), name)
+
+    def get_fund_strategy(self, fund_name: str, strategy_name: str, business_datetime: datetime,
+                          system_datetime: datetime) -> FundStrategy:
+        """select * from fund_strategy where id = fund_strategy_id"""
+        (s_user_email, s_user_id, s_user_name, s_name, description, fs_user_email, fs_user_id, fs_user_name, currency,
+         f_name, save_output_flag, weight) = self.call_proc(Proc.select_fund_strategy.name,
+                                                            [fund_name, strategy_name, business_datetime,
+                                                             system_datetime])
+        s_user = User(s_user_email, s_user_id, s_user_name)
+        s = Strategy(s_name, s_user, description)
+        fs_user = User(fs_user_email, fs_user_id, fs_user_name)
+        f = Fund(Currency(currency), f_name)
+        return FundStrategy(business_datetime, f, save_output_flag, s, weight, fs_user)
