9bfc8820835566812d8f82bd4bac531eb8b277e8
Anais Jeremie <anais.jeremie@lgim.com>
2020-07-21 11:28:34 +0200

    Completed Warning Flags Rates

--- assetallocation_arp/models/effect/compute_warning_flags_overview.py
+++ assetallocation_arp/models/effect/compute_warning_flags_overview.py
@@ -1,4 +1,5 @@
 
+
 class ComputeWarningFlagsOverview:
 
     def __init__(self, latest_date, previous_seven_days_latest_date):
@@ -7,10 +8,17 @@ class ComputeWarningFlagsOverview:
 
     def compute_warning_flags_rates(self, currency_three_month_implied_usd, currency_three_month_implied_eur):
 
-        three_month_implied_latest_date = currency_three_month_implied_usd.loc[self.latest_date]
-        three_month_implied_previous_seven_days_latest_date = currency_three_month_implied_usd.loc[self.seven_previous_latest_date]
+        three_month_implied_usd_latest_date = currency_three_month_implied_usd.loc[self.latest_date]
+        three_month_implied_usd_previous_seven_days_latest_date = currency_three_month_implied_usd.loc[self.seven_previous_latest_date]
 
-        return three_month_implied_latest_date - three_month_implied_previous_seven_days_latest_date
+        rates_usd = three_month_implied_usd_latest_date - three_month_implied_usd_previous_seven_days_latest_date
+
+        three_month_implied_eur_latest_date = currency_three_month_implied_eur.loc[self.latest_date]
+        three_month_implied_eur_previous_seven_days_latest_date = currency_three_month_implied_eur.loc[self.seven_previous_latest_date]
+
+        rates_eur = three_month_implied_eur_latest_date - three_month_implied_eur_previous_seven_days_latest_date
+
+        return rates_usd, rates_eur
 
     def compute_warning_flags_inflation(self):
         pass
