9f40da15ff1b2f28896d0ff37ec5ed99600eed79
JS89275 <jessica.smart@lgim.com>
2020-07-27 13:25:15 +0100

    Add asset_tickers as input to insert_times_strategy

--- assetallocation_arp/data_etl/dal/temp_sql/insert_times_strategy.sql
+++ assetallocation_arp/data_etl/dal/temp_sql/insert_times_strategy.sql
@@ -9,7 +9,8 @@ CREATE OR REPLACE FUNCTION insert_times_strategy(
   long_signals numeric[],
   frequency frequency,
   day_of_week int,
-  OUT version int
+  asset_tickers varchar[],
+  OUT t_version int
 )
 LANGUAGE plpgsql
 AS
@@ -43,6 +44,10 @@ BEGIN
 	  strategy_id
 	)
 	RETURNING arp.times.version into t_version;
+  WITH a (asset_id) as (select id from asset.asset where ticker = ANY(asset_tickers))
+  INSERT INTO arp.times_asset (strategy_id, asset_id, execution_state_id)
+  SELECT strategy_id, a.asset_id, execution_state_id
+  FROM a;
 	return;
 END
 $$;
