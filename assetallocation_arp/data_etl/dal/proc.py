4adcd568bdf5c72aefe9e0ae3454c8b100f21241
JS89275 <jessica.smart@lgim.com>
2020-07-27 15:34:34 +0100

    Add insert_fund sql function

--- assetallocation_arp/data_etl/dal/proc.py
+++ assetallocation_arp/data_etl/dal/proc.py
@@ -4,6 +4,7 @@ from enum import Enum, auto
 class Proc(Enum):
     select_fund = auto()
     select_fund_strategy = auto()
+    insert_fund = auto()
     insert_fund_strategy = auto()
     insert_effect_strategy = auto()
     insert_fica_strategy = auto()
