5a9ac7e33d1f975b47a627ab9af452e3916ecbf8
Anais Jeremie <anais.jeremie@lgim.com>
2020-08-06 13:55:13 +0200

    Adding calculations for risk/returns module

--- assetallocation_arp/models/compute_risk_return_calculations.py
+++ assetallocation_arp/models/compute_risk_return_calculations.py
@@ -1,4 +1,4 @@
-import numpy as np
+import pandas as pd
 from math import sqrt
 
 
@@ -12,41 +12,35 @@ class ComputeRiskReturnCalculations:
     def compute_no_signals_excess_returns(self, returns_excl_signals):
         exp = 365 / (self.end_date - self.start_date).days
 
-        return ((returns_excl_signals.loc[self.end_date][0]/returns_excl_signals.loc[self.start_date][0] ** exp) - 1) * 100
+        return (((returns_excl_signals.loc[self.end_date][0]/returns_excl_signals.loc[self.start_date][0]) ** exp) - 1) * 100
 
-    def compute_no_signals_std_dev(self, returns_excl_signals):
-        next_start_date = self.dates_index[np.where(self.dates_index == self.start_date)[0][0] + 1]
-        before_last_date = self.dates_index[np.where(self.dates_index == self.end_date)[0][0] - 1]
+    @staticmethod
+    def compute_no_signals_std_dev(returns_excl_signals):
 
-        (returns_excl_signals / returns_excl_signals.shift(1)).std() * sqrt(52)
-
-        (returns_excl_signals[next_start_date:] / returns_excl_signals[self.start_date:before_last_date]).std() * sqrt(52)
-
-        return 1
+        return (returns_excl_signals / returns_excl_signals.shift(1)).std()[0] * sqrt(52) * 100
 
     @staticmethod
     def compute_no_signals_sharpe_ratio(excess_returns, std_dev):
 
-        return excess_returns - std_dev
+        return excess_returns / std_dev
 
-    def compute_no_signals_max_drawdown(self):
-        pass
-        # # find out the max value between t_start and t_end
-        # max_price = pd.to_numeric(self.time_series[t_start:self.t_end]).max()
-        #
-        # # find out the max date
-        # max_date_loc = pd.Index(pd.to_numeric(self.time_series[t_start:self.t_end])).get_loc(max_price)
-        # t_max = self.time_series[t_start:self.t_end].index[max_date_loc]
-        #
-        # # find out the min value between t_max and date_end
-        # min_price = pd.to_numeric(self.time_series[t_max:self.t_end]).min()
-        #
-        # # compute the maximum drawdown
-        # self.maximum_drawdown.append((((min_price / max_price) - 1) * 100))
+    def compute_no_signals_max_drawdown(self, returns_excl_signals):
+
+        # find out the max value between t_start and t_end
+        max_price = (returns_excl_signals[self.start_date:self.end_date]).max()[0]
+        # find out the max date
+        max_price_date = returns_excl_signals[returns_excl_signals.Total_Excl_Signals == max_price].index.values[0]
+
+        # find out the min value between t_max and date_end
+        min_price = (returns_excl_signals[max_price_date:self.end_date]).min()[0]
+
+        # compute the maximum drawdown
+        return ((min_price / max_price) - 1) * 100
 
     @staticmethod
     def compute_no_signals_calmar_ratio(excess_returns, max_drawdown):
-        pass
+
+        return excess_returns / max_drawdown
 
     def compute_no_signals_equity_correlation(self):
         pass
@@ -55,9 +49,10 @@ class ComputeRiskReturnCalculations:
         pass
 
     def run_compute_risk_return_calculations(self, returns_excl_signals):
+        returns_excl_signals.to_csv('returns_exl_sign.csv')
         excess_returns = self.compute_no_signals_excess_returns(returns_excl_signals)
         std_dev = self.compute_no_signals_std_dev(returns_excl_signals)
         sharpe_ratio = self.compute_no_signals_sharpe_ratio(excess_returns, std_dev)
-        max_drawdown = self.compute_no_signals_max_drawdown()
-        calmar_ratio = self.compute_no_signals_calmar_ratio()
+        max_drawdown = self.compute_no_signals_max_drawdown(returns_excl_signals)
+        calmar_ratio = self.compute_no_signals_calmar_ratio(excess_returns, max_drawdown)
 
