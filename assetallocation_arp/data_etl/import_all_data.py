63c3b1f6d87c5e4ac393801675394fd753e466ba
Anais Jeremie <anais.jeremie@lgim.com>
2020-08-10 20:06:29 +0200

    Added a function to recognize the plaform

--- assetallocation_arp/data_etl/import_all_data.py
+++ assetallocation_arp/data_etl/import_all_data.py
@@ -80,8 +80,11 @@ def data_frame_from_xlsx(xlsx_file, range_name, hascolnames):
 
 
 def extract_inputs_and_mat_data(model_type, mat_file=None, input_file=None, model_date=None):
-    file_path = '/domino/datasets/local/matlab_data.csv'
 
+    if sys.platform == "linux2":
+        file_path = '/domino/datasets/local/matlab_data.csv'
+    else:
+        file_path = 'S:/Shared/IT/MultiAsset/Data/Arquive/matlabData.mat'
     # if mat_file is None:
     #     print(sys.platform)
     #     if sys.platform == "Linux":
