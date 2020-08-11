2c09690debe0532302cd2cb8a361a6aa8a1d1ff4
Anais Jeremie <anais.jeremie@lgim.com>
2020-08-11 19:09:37 +0200

    Changed the path to save the model results in Domino

--- assetallocation_UI/aa_web_app/routes.py
+++ assetallocation_UI/aa_web_app/routes.py
@@ -101,7 +101,7 @@ def times_page():
             save_file = "save_file"
             name_of_file = form.name_file_times.data + ".xls"
             #TODO change the default location later
-            path_excel = '/mnt/models_results'
+            path_excel = '/mnt'
             # path_excel = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", '..', '..'))
             path_excel_times = path_excel + "/" + name_of_file
 
