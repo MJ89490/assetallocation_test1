945932c081d9cf3d629acb4cf4cbe48548f2057f
Anais Jeremie <anais.jeremie@lgim.com>
2020-08-13 09:18:22 +0200

    Proper file for Domino

--- assetallocation_arp/arp_strategies.py
+++ assetallocation_arp/arp_strategies.py
@@ -45,11 +45,13 @@ def write_output_to_excel(model_outputs, path_excel_times):
 
 	if models.times.name in model_outputs.keys():
 		positioning, returns, signals = model_outputs[models.times.name]
-		with pd.ExcelWriter(path_excel_times) as writer:
+		with pd.ExcelWriter(path_excel_times, engine='xlswriter') as writer:
 			signals.to_excel(writer, sheet_name='signal', encoding='utf8')
 			returns.to_excel(writer, sheet_name='returns', encoding='utf8')
 			positioning.to_excel(writer, sheet_name='positioning', encoding='utf8')
 			writer.save()
+			writer.close()
+		writer.save()
 
 def get_inputs_from_python(model):
 	#launch the script from Python
