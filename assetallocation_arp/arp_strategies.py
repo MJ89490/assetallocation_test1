f3cacdb9802ac5e237d7e34a9f8ae5786a22a1f7
Manimegalai Jaganathan <Manimegalai.Jaganathan@lgim.com>
2020-08-13 11:15:50 +0100

    debugging the values

--- assetallocation_arp/arp_strategies.py
+++ assetallocation_arp/arp_strategies.py
@@ -47,7 +47,7 @@ def write_output_to_excel(model_outputs, path_excel_times):
         print("===models.times.name===", models.times.name)
         print("=======model output keys===", model_outputs.keys())
         positioning, returns, signals = model_outputs[models.times.name]
-        print("===========current _path ===========", os.getcwd())
+        print("===========current _path, excel path ===========", os.getcwd(), path_excel_times)
         with pd.ExcelWriter(path_excel_times) as writer:
             signals.to_excel(writer, sheet_name='signal', encoding='utf8')
             returns.to_excel(writer, sheet_name='returns', encoding='utf8')
