18a9bd7e58ca43906ed929869658d8bc030e23c6
JS89275 <jessica.smart@lgim.com>
2020-08-13 14:02:20 +0100

    Add function select_times_assets_with_analytics

--- assetallocation_arp/data_etl/dal/database_scripts/functions/arp/select_times_assets_with_analytics.sql
+++ assetallocation_arp/data_etl/dal/database_scripts/functions/arp/select_times_assets_with_analytics.sql
@@ -1,5 +1,6 @@
-CREATE OR REPLACE FUNCTION arp.select_times_assets(
-  strategy_version int
+CREATE OR REPLACE FUNCTION arp.select_times_assets_with_analytics(
+  strategy_version int,
+  business_datetime timestamp with time zone
 )
   RETURNS TABLE(
     category varchar,
@@ -13,7 +14,8 @@ CREATE OR REPLACE FUNCTION arp.select_times_assets(
     ticker varchar,
     is_tr boolean,
     asset_type varchar,
-    s_leverage integer
+    s_leverage integer,
+    asset_analytics arp.source_category_value[]
   )
 LANGUAGE plpgsql
 AS
@@ -36,7 +38,8 @@ BEGIN
       a.ticker,
       a.is_tr,
       a.type as asset_type,
-      a.s_leverage
+      a.s_leverage,
+      array_agg((s2.source, aa.category, aa.value) :: arp.source_category_value) as asset_analytic
     FROM
       asset.asset a
       JOIN arp.times_asset ta on a.id = ta.asset_id
@@ -44,10 +47,12 @@ BEGIN
       JOIN arp.strategy s on t.strategy_id = s.id
       JOIN lookup.currency c on a.currency_id = c.id
       JOIN lookup.country c2 on a.country_id = c2.id
+      JOIN asset.asset_analytic aa on a.id = aa.asset_id
       JOIN lookup.source s2 on aa.source_id = s2.id
     WHERE
       s.name = strategy_name
       AND t.version = strategy_version
+      AND aa.business_tstzrange @> business_datetime
     GROUP BY a.id, c.id, c2.id
   ;
 END
