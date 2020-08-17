265656d4c143e113a734bbffe6d09592d7f636de
JS89275 <jessica.smart@lgim.com>
2020-08-17 10:57:37 +0100

    Supress PyArgumentList inspections on calls to Enum

--- assetallocation_arp/common_libraries/dal_enums/asset.py
+++ assetallocation_arp/common_libraries/dal_enums/asset.py
@@ -1,6 +1,6 @@
 from enum import Enum, auto
 
-
+# noinspection PyArgumentList
 Category = Enum(
     value='Category',
     names=[
--- assetallocation_arp/data_etl/dal/proc.py
+++ assetallocation_arp/data_etl/dal/proc.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 
+# noinspection PyArgumentList
 Proc = Enum(
     value='Proc',
     names=[
