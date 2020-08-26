9b76c4d5b4918b71fca3a4be2a4e01bad46a0dd5
Simone Nascimento <Simone.Nascimento@lgim.com>
2020-08-26 15:58:45 +0100

    Merged all models into Development - Adjusted excel file to run the model selected up to the date selected and added the option to run NAAWE

--- assetallocation_arp/arp_strategies.py
+++ assetallocation_arp/arp_strategies.py
@@ -3,11 +3,11 @@ import sys
 import xlwings as xw
 from time import strftime, gmtime
 
-from common_libraries.dal_enums.strategy import Name
-
 ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 print(ROOT_DIR)
 sys.path.insert(0, ROOT_DIR)
+
+from common_libraries.dal_enums.strategy import Name
 from assetallocation_arp.data_etl import import_data_from_excel_matlab as gd
 from assetallocation_arp.models import times
 from assetallocation_arp.models import fica
