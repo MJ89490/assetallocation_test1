5df73b18cbb1a4631ccc119f0130765d551c32b3
Anais Jeremie <anais.jeremie@lgim.com>
2020-09-07 16:15:15 +0200

    Link python code to excel

---
+++ assetallocation_arp/data_etl/inputs_effect/write_inputs_effect_excel.py
@@ -0,0 +1,67 @@
+from configparser import ConfigParser
+import xlwings as xw
+import pandas as pd
+from datetime import timedelta
+import os
+
+
+def get_inputs_effect_excel(input_file):
+    xw.Book(input_file).set_mock_caller()
+
+    sheet_effect_input = xw.Book.caller().sheets['effect_input']
+
+    user_date = sheet_effect_input.range('start_date_calculations_effect').value
+
+    # EFFECT inputs
+
+    if user_date is None:
+        # Instantiate ConfigParser
+        config = ConfigParser()
+        # Parse existing file
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'config_effect_model', 'dates_effect.ini'))
+        config.read(path)
+        # Read values from the dates_effect.ini file
+        default_start_date = config.get('start_date_computations', 'start_date_calculations')
+        user_date = default_start_date
+
+    realtime_inflation_forecast = sheet_effect_input.range('real_time_inf').value.strip().lower()
+    trend = sheet_effect_input.range('trend_indicator_input').value
+    short_term = sheet_effect_input.range('short_term_input').value
+    long_term = sheet_effect_input.range('long_term_input').value
+    trend_inputs = {'short_term': int(short_term), 'long_term': int(long_term), 'trend': trend.strip().lower()}
+
+    incl_shorts = sheet_effect_input.range('incl_shorts_input').value
+    cut_off = sheet_effect_input.range('cut_off_long_input').value * 100
+    cut_off_s = sheet_effect_input.range('cut_off_short_input').value * 100
+    thr = sheet_effect_input.range('threshold_closing_input').value * 100
+
+    combo_inputs = {'cut_off': float(cut_off), 'incl_shorts': incl_shorts.strip().lower(),
+                    'cut_off_s': float(cut_off_s), 'threshold': float(thr)}
+
+    carry = sheet_effect_input.range('type_carry_input').value
+
+    carry_inputs = {'type': carry.strip().lower(), 'inflation': ''}
+
+    window_size = sheet_effect_input.range('window_input').value
+    weight = sheet_effect_input.range('weight_input').value
+    position_size_attribution = sheet_effect_input.range('pos_attr_input').value
+    bid_ask_spread = sheet_effect_input.range('bid_ask_input').value
+
+    weighting_costs = {'window': int(window_size), 'weight': weight, 'pos_size_attr': float(position_size_attribution),
+                       'bid_ask': int(bid_ask_spread)}
+
+    return user_date, trend_inputs, combo_inputs, carry_inputs, realtime_inflation_forecast, weighting_costs
+
+
+def get_latest_date_signal_excel(obj_import_data: object):
+
+    sheet_effect_input = xw.Book.caller().sheets['effect_input']
+    latest_signal_date = sheet_effect_input.range('latest_signal_date').value
+
+    if latest_signal_date is None:
+        # latest_signal_date = pd.to_datetime('15-07-2020', format='%d-%m-%Y')
+        latest_signal_date = obj_import_data.dates_origin_index[-1]
+        offset = (latest_signal_date.weekday() - 2) % 7
+        latest_signal_date = pd.to_datetime(latest_signal_date - timedelta(days=offset), format='%d-%m-%Y')
+
+    return latest_signal_date
