5c1baa4a9a403b497453535d4ed144394f5af4d3
JS89275 <jessica.smart@lgim.com>
2020-09-23 13:34:17 +0100

    Add tests for EffectProcCaller

--- tests/assetallocation_arp/data_etl/dal/test_arp_proc_caller.py
+++ tests/assetallocation_arp/data_etl/dal/test_arp_proc_caller.py
@@ -1,7 +1,8 @@
 import mock
 import pytest
 
-from assetallocation_arp.data_etl.dal.arp_proc_caller import TimesProcCaller, Times, TimesAssetInput, FundStrategy
+from assetallocation_arp.data_etl.dal.arp_proc_caller import TimesProcCaller, Times, TimesAssetInput, FundStrategy, \
+    EffectProcCaller, Effect, ArpProcCaller, EffectAssetInput
 from datetime import datetime
 
 
@@ -13,7 +14,7 @@ def mock_engine():
 
 @pytest.fixture(autouse=True)
 def mock_call_proc():
-    with mock.patch.object(TimesProcCaller, 'call_proc', autospec=True) as _mock_call_proc:
+    with mock.patch.object(ArpProcCaller, 'call_proc', autospec=True) as _mock_call_proc:
         yield _mock_call_proc
 
 
@@ -21,6 +22,12 @@ def mock_call_proc():
 def MockTimes():
     with mock.patch('assetallocation_arp.data_etl.dal.arp_proc_caller.Times', autospec=True) as _MockTimes:
         yield _MockTimes
+
+
+@pytest.fixture()
+def MockEffect():
+    with mock.patch('assetallocation_arp.data_etl.dal.arp_proc_caller.Effect', autospec=True) as _MockEffect:
+        yield _MockEffect
 
 
 def test_insert_times_strategy_calls_call_proc(MockTimes, mock_call_proc):
@@ -142,7 +149,7 @@ def test_select_fund_strategy_results_calls_call_proc(mock_call_proc):
                                            [fund_name, strategy_name, strategy_version])
 
 
-def test_select_times_assets_returns_fund_strategy(mock_call_proc):
+def test_select_fund_strategy_results_returns_fund_strategy(mock_call_proc):
     fund_name = 'b'
     strategy_name = 'times'
     strategy_version = 1
@@ -153,3 +160,76 @@ def test_select_times_assets_returns_fund_strategy(mock_call_proc):
         returns = a.select_fund_strategy_results(fund_name, strategy_name, strategy_version)
 
     assert isinstance(returns, FundStrategy)
+
+
+def test_insert_effect_strategy_calls_call_proc(MockEffect, mock_call_proc):
+    mock_effect = MockEffect('Nominal', 1, 1, 1, 'monthly', 1, 1, 1, 1, 1, 1, 1, 'TotalReturn')
+    user_id = 'a'
+
+    a = EffectProcCaller()
+    a._insert_effect_strategy(mock_effect, user_id)
+
+    mock_call_proc.assert_called_once_with(a, 'arp.insert_effect_strategy',
+                                           [mock_effect.description, 'a', mock_effect.carry_type.name,
+                                            mock_effect.closing_threshold, mock_effect.cost,
+                                            mock_effect.day_of_week.value, mock_effect.frequency.name,
+                                            mock_effect.include_shorts, mock_effect.inflation_lag_interval,
+                                            mock_effect.interest_rate_cut_off_long,
+                                            mock_effect.interest_rate_cut_off_short,
+                                            mock_effect.moving_average_long_term, mock_effect.moving_average_short_term,
+                                            mock_effect.is_realtime_inflation_forecast, mock_effect.trend_indicator.name
+                                            ])
+
+
+def test_insert_effect_strategy_returns_e_version(MockEffect, mock_call_proc):
+    mock_effect = MockEffect('Nominal', 1, 1, 1, 'monthly', 1, 1, 1, 1, 1, 1, 1, 'TotalReturn')
+    expected = 5
+    mock_call_proc.return_value = [{'e_version': expected}]
+
+    a = EffectProcCaller()
+    returns = a._insert_effect_strategy(mock_effect, 'a')
+
+    assert expected == returns
+
+
+def test_select_effect_strategy_calls_call_proc(mock_call_proc):
+    e_version = 2
+    mock_call_proc.return_value = []
+
+    a = EffectProcCaller()
+    a._select_effect_strategy(e_version)
+
+    mock_call_proc.assert_called_once_with(a, 'arp.select_effect_strategy', [e_version])
+
+
+def test_select_effect_strategy_returns_effect_object(MockEffect):
+    a = EffectProcCaller()
+
+    returns = a._select_effect_strategy(2)
+
+    assert isinstance(returns, Effect)
+
+
+def test_select_effect_assets_with_analytics_calls_call_proc(mock_call_proc):
+    e_version = 2
+    business_datetime = datetime(2020, 9, 1)
+    mock_call_proc.return_value = []
+
+    a = EffectProcCaller()
+    a.select_effect_assets_with_analytics(e_version, business_datetime)
+
+    mock_call_proc.assert_called_once_with(a, 'arp.select_effect_assets_with_analytics', [e_version, business_datetime])
+
+
+def test_select_effect_assets_returns_list_of_effect_asset_objects(mock_call_proc):
+    e_version = 2
+    business_datetime = datetime(2020, 9, 1)
+    mock_call_proc.return_value = [mock.MagicMock(), mock.MagicMock(), mock.MagicMock()]
+
+    with mock.patch('assetallocation_arp.data_etl.dal.arp_proc_caller.EffectAssetInput', autospec=True):
+        a = EffectProcCaller()
+        returns = a.select_effect_assets_with_analytics(e_version, business_datetime)
+
+    assert isinstance(returns, list)
+    assert all([isinstance(i, EffectAssetInput) for i in returns])
+
