0d21a14774038b4bcb5a32909edbeaf17dad5e34
Anais Jeremie <anais.jeremie@lgim.com>
2020-11-30 19:40:07 +0100

    Added unit tests for ImportDataEffect

--- tests/assetallocation_arp/models/effect/test_import_data_effect.py
+++ tests/assetallocation_arp/models/effect/test_import_data_effect.py
@@ -3,13 +3,6 @@ import pytest
 import pandas as pd
 from assetallocation_arp.data_etl.inputs_effect.import_data_effect import ImportDataEffect
 
-path_all_data = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "resources", "effect", "outputs_origin", "all_date.csv"))
-all_data = pd.read_csv(path_all_data, sep=',', engine='python')
-
-end_date_mat, start_date_mat, frequency, signal_day_mat, all_data = '23/09/2020', '06/01/1999', 'weekly', 'WED', all_data
-
-obj_import_data_effect = ImportDataEffect(end_date_mat, start_date_mat, frequency, signal_day_mat, all_data)
-
 
 @pytest.mark.parametrize("last_date_origin, last_date_expected, freq",
                          [("15-09-2020", "22-09-2020", "weekly"),
@@ -21,5 +14,11 @@ obj_import_data_effect = ImportDataEffect(end_date_mat, start_date_mat, frequenc
                           ("30-09-2020", "30-10-2020", "monthly"),
                           ("31-12-2020", "29-01-2021", "monthly")])
 def test_add_next_date(last_date_origin, last_date_expected, freq):
-    last_date = obj_import_data_effect.add_next_date(freq, pd.to_datetime(last_date_origin, format='%d-%m-%Y'))
+    all_data = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "resources", "effect", "outputs_origin", "all_date.csv")), sep=',', engine='python')
+    all_data = all_data.set_index(pd.to_datetime(all_data.Date, format='%Y-%m-%d'))
+    del all_data['Date']
+
+    obj_import_data = ImportDataEffect(pd.to_datetime('23/09/2020', format='%d/%m/%Y'), "weekly", "WED", all_data)
+
+    last_date = obj_import_data.add_next_date(freq, pd.to_datetime(last_date_origin, format='%d-%m-%Y'))
     assert last_date == pd.to_datetime(last_date_expected, format='%d-%m-%Y')
