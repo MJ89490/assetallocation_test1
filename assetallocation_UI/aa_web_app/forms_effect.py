1d18ba7210b77cfa84a72da3062a8bf6e14cf22f
Anais Jeremie <anais.jeremie@lgim.com>
2020-12-04 09:51:20 +0100

    Added fund name button for effect

--- assetallocation_UI/aa_web_app/forms_effect.py
+++ assetallocation_UI/aa_web_app/forms_effect.py
@@ -3,10 +3,16 @@ from wtforms import SelectField, SubmitField, StringField
 
 
 class InputsEffectStrategy(FlaskForm):
+    # Versions
     version_choices = [('New Version', 'New Version')]  # TODO to automate
     versions_effect = SelectField('Versions', choices=version_choices)
     submit_ok_charts_effect_data = SubmitField('ok')
 
+    # Fund name
+    fund_names = [('f1', 'f1')]  # TODO to automate
+    input_fund_name_effect = SelectField('Fund Names', choices=fund_names)
+
+    # Start and end date
     start_date_effect_inputs = StringField('Start Date')
     end_date_effect_inputs = StringField('End Date')
     submit_ok_export_effect_data = SubmitField('ok')
