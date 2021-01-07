0337363c797bff3d80c7850e3fe5467cc683986b
Anais Jeremie <anais.jeremie@lgim.com>
2021-01-07 19:32:01 +0100

    Added error handling for charts sidebar

--- assetallocation_UI/aa_web_app/static/js/form_dashboard_times.js
+++ assetallocation_UI/aa_web_app/static/js/form_dashboard_times.js
@@ -1,12 +1,34 @@
-data_to_send_to_python = []
+version_data_to_send_to_python = []
+fund_data_to_send_to_python = []
 ready_to_send = false
 type_of_request = 'charts_data_sidebar'
 
 function sideBarFund() {
-//CREATE ERROR HANDLING BECAUSE WE CANNOT HAVE MORE THAN 1 FUND
         var select_box = document.getElementById("send_data_fund_from_sidebar");
         var fund = select_box.options[select_box.selectedIndex].value;
-        this.sendDataToPython(fund);
+
+        fund_data_to_send_to_python.push(fund);
+        console.log(fund_data_to_send_to_python.length);
+
+        if (fund_data_to_send_to_python.length != 1){
+            fund_data_to_send_to_python.splice(0, fund_data_to_send_to_python.length-1);
+        }
+}
+
+function sideBarVersion() {
+        var select_box = document.getElementById("send_data_version_from_sidebar");
+        var version = select_box.options[select_box.selectedIndex].value;
+        ready_to_send = true;
+        version_data_to_send_to_python.push(parseInt(version));
+
+        console.log(version_data_to_send_to_python);
+
+        if (version_data_to_send_to_python.length != 1){
+            version_data_to_send_to_python.splice(0, version_data_to_send_to_python.length-1);
+        }
+
+        this.sendDataToPython();
+
 }
 
 function sideBarExportFund() {
@@ -14,13 +36,6 @@ function sideBarExportFund() {
         var select_box = document.getElementById("send_data_fund_export_from_sidebar");
         var fund = select_box.options[select_box.selectedIndex].value;
         this.sendDataToPython(fund);
-}
-
-function sideBarVersion() {
-        var select_box = document.getElementById("send_data_version_from_sidebar");
-        var version = select_box.options[select_box.selectedIndex].value;
-        ready_to_send = true;
-        sendDataToPython(parseInt(version));
 }
 
 function sideBarExportVersion() {
@@ -31,13 +46,13 @@ function sideBarExportVersion() {
         sendDataToPython(parseInt(version));
 }
 
-function sendDataToPython(val){
-    data_to_send_to_python.push(val);
-    console.log(data_to_send_to_python);
+function sendDataToPython(){
 
     if (ready_to_send == true){
-        var json_data = JSON.stringify({"inputs_sidebar": data_to_send_to_python, "type_of_request": type_of_request});
+
+        var json_data = JSON.stringify({"input_fund": fund_data_to_send_to_python, "inputs_version": version_data_to_send_to_python, "type_of_request": type_of_request});
         console.log(json_data);
+
         $.ajax({
                 url: "receive_sidebar_data_times_form",
                 data: {json_data: json_data},
