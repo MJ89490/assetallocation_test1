87c4128ed6d0cd62aa399824a77499cc3a79391d
JS89275 <jessica.smart@lgim.com>
2021-01-25 14:21:00 +0000

    Update audit triggers to be on asset.asset and asset.asset_group

--- assetallocation_arp/data_etl/dal/database_scripts/triggers/audit/audit_triggers.sql
+++ assetallocation_arp/data_etl/dal/database_scripts/triggers/audit/audit_triggers.sql
@@ -1,3 +1,2 @@
 SELECT audit.audit_table('asset.asset', 't', 't');
-SELECT audit.audit_table('asset.asset_analytic', 't', 't');
-SELECT audit.audit_table('arp.times_asset', 't', 't');
+SELECT audit.audit_table('asset.asset_group', 't', 't');
