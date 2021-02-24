eca89297da125621177aeca20be180e4541c9c7e
JS89275 <jessica.smart@lgim.com>
2021-02-24 15:48:46 +0000

    Add config.asset_analytic table with static data

--- assetallocation_arp/data_etl/dal/database_scripts/static_data/config/execution.sql
+++ assetallocation_arp/data_etl/dal/database_scripts/static_data/config/execution.sql
@@ -14,6 +14,7 @@ VALUES
   ('fund.insert_fund', 'Insert fund', 't'),
   ('arp.insert_app_user', 'Insert app user into arp.app_user', 't'),
   ('config.insert_model', 'Insert model into config.model', 't'),
+  ('config.insert_asset_analytic', 'Insert asset analytic into config.asset_analytic', 't'),
   ('staging.load_assets', 'Load assets from staging.asset into asset.asset, asset.asset_group and asset.asset_analytic', 't'),
   ('staging.load_asset_analytics', 'Load asset_analytics from staging.asset_analytic into asset.asset_analytic', 't')
 ON CONFLICT DO NOTHING
