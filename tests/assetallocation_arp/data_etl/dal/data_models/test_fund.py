5d81826b2defb6c8c30903790402aba3d544044e
JS89275 <jessica.smart@lgim.com>
2021-03-01 11:48:36 +0000

    Update tests using currency and country Enums

--- tests/assetallocation_arp/data_etl/dal/data_models/test_fund.py
+++ tests/assetallocation_arp/data_etl/dal/data_models/test_fund.py
@@ -1,6 +1,7 @@
 from pytest import raises
 
 from assetallocation_arp.data_etl.dal.data_models.fund import Fund
+from assetallocation_arp.common_libraries.dal_enums.currency import Currency
 
 
 def test_currency_setter_raises_key_error_invalid_currency():
@@ -9,7 +10,7 @@ def test_currency_setter_raises_key_error_invalid_currency():
 
 
 def test_currency_setter_sets_currency_valid_currency():
-    currency = 'EUR'
+    currency = Currency.EUR.name
 
     f = Fund('a', currency)
 
