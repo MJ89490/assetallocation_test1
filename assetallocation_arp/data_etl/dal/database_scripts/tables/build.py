b7179ede8c372c660cabff2d9c4b1910f48a2396
JS89275 <jessica.smart@lgim.com>
2021-03-12 11:10:12 +0000

    Add auth schema with user role and permission tables

--- assetallocation_arp/data_etl/dal/database_scripts/tables/build.py
+++ assetallocation_arp/data_etl/dal/database_scripts/tables/build.py
@@ -8,17 +8,20 @@ def get_table_sql_files() -> List[str]:
     a = p / 'asset'
     ar = p / 'arp'
     au = p / 'audit'
+    aut = p / 'auth'
     co = p / 'config'
     f = p / 'fund'
     lo = p / 'lookup'
 
     files = [lo / 'country.sql', lo / 'currency.sql', lo / 'source.sql', co / 'execution.sql',
              co / 'execution_state.sql', co / 'model.sql', co / 'model_instance.sql',
-             f / 'fund.sql', au / 'logged_action.sql', a / 'asset_group.sql', a / 'asset.sql', a / 'asset_analytic.sql',
-             ar / 'app_user.sql', ar / 'strategy.sql', ar / 'effect.sql', ar / 'fica.sql', ar / 'times.sql',
-             ar / 'fx.sql', ar / 'maven.sql', ar / 'strategy_asset_group.sql', ar / 'effect_asset_group.sql',
-             ar / 'times_asset_group.sql', ar / 'fx_asset_group.sql', ar / 'maven_asset_group.sql',
-             ar / 'strategy_asset.sql', ar / 'strategy_asset_analytic.sql', ar / 'strategy_analytic.sql',
-             ar / 'strategy_asset_weight.sql', ar / 'fund_strategy_asset_weight.sql', ar / 'fund_strategy_weight.sql']
+             aut / 'role.sql', aut / 'permission.sql', aut / 'user.sql', aut / 'role_permission.sql',
+             aut / 'user_role.sql', f / 'fund.sql', au / 'logged_action.sql', a / 'asset_group.sql', a / 'asset.sql',
+             a / 'asset_analytic.sql', ar / 'app_user.sql', ar / 'strategy.sql', ar / 'effect.sql', ar / 'fica.sql',
+             ar / 'times.sql', ar / 'fx.sql', ar / 'maven.sql', ar / 'strategy_asset_group.sql',
+             ar / 'effect_asset_group.sql', ar / 'times_asset_group.sql', ar / 'fx_asset_group.sql',
+             ar / 'maven_asset_group.sql', ar / 'strategy_asset.sql', ar / 'strategy_asset_analytic.sql',
+             ar / 'strategy_analytic.sql', ar / 'strategy_asset_weight.sql', ar / 'fund_strategy_asset_weight.sql',
+             ar / 'fund_strategy_weight.sql']
 
     return files
