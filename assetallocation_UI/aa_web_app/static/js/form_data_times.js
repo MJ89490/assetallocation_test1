7431b4455b0572242aab804a34f03cf101b2b1d8
Anais Jeremie <anais.jeremie@lgim.com>
2021-03-21 15:08:05 +0000

    Testing run strategy with jquery table

--- assetallocation_UI/aa_web_app/static/js/form_data_times.js
+++ assetallocation_UI/aa_web_app/static/js/form_data_times.js
@@ -1,132 +1,49 @@
-
-var rowData = [
-  {asset: 'DM FX', category: 'FX', signal_ticker: 'SPXT Index',future_ticker: 'CD1 A:00_0_R Curncy', costs:  0.0002, s_leverage: 1},
-  {asset: 'DM Equity', category: 'Equities', signal_ticker: 'TPXDDVD Index', future_ticker: 'SPXT Index', costs:  0.0002, s_leverage: 1},
-  {asset: 'Nominal Bond', category: 'Fixed Income', signal_ticker: 'G 1 A:00_0_R Comdty', future_ticker: 'CN1 A:00_0_R Comdty', costs:  0.0002, s_leverage: 1},
-
-//  {asset: 'EU Equities', category: 'Equities', signal_ticker: 'SX5T Index', future_ticker: 'AD1 A:00_0_R Curncy', costs:  0.0002, s_leverage: 1},
-//
-//  {asset: 'HK Equities', category: 'Equities', signal_ticker: 'HSI 1 A:00_0_R Index', future_ticker: 'B1 A:00_0_R Curncy', costs:  0.0002, s_leverage: 1},
-//  {asset: 'US 10y Bonds', category: 'Bonds', signal_ticker: 'MLT1US10 Index', future_ticker: 'SPXT Index', costs:  0.0002, s_leverage: 1},
-//
-//  {asset: 'EU 10y Bonds', category: 'Bonds', signal_ticker: 'SRX1 A:00_0_R Comdty', future_ticker: 'SPXT Index', costs:  0.0002, s_leverage: 1},
-//  {asset: 'JPY', category: 'FX', signal_ticker: 'JPYUSD Curncy', future_ticker: 'SPXT Index', costs:  0.0002, s_leverage: 1},
-//  {asset: 'EUR', category: 'FX', signal_ticker: 'EURUSD Curncy', future_ticker: 'SPXT Index', costs:  0.0002, s_leverage: 1},
-//  {asset: 'AUD', category: 'FX', signal_ticker: 'AUDUSD Curncy', future_ticker: 'AUDUSD Curncy', costs:  0.0002, s_leverage: 1},
-//  {asset: 'CAD', category: 'FX', signal_ticker: 'CADUSD Curncy', future_ticker: 'SPXT Index', costs:  0.0002, s_leverage: 1},
-//  {asset: 'GBP', category: 'FX', signal_ticker: 'EURGBP Curncy', future_ticker: 'SPXT Index', costs:  0.0002, s_leverage: 1}
-];
-
-var gridOptions = {
-  columnDefs: [
-    { headerName: 'Asset', field: 'asset' },
-    { headerName: 'Category', field: 'category' },
-    { headerName: 'Signal Ticker', field: 'signal_ticker' },
-    { headerName: 'Future Ticker', field: 'future_ticker' },
-    { headerName: 'Costs', field: 'costs' },
-    { headerName: 'Leverage', field: 's_leverage' }
-  ],
-  defaultColDef: {flex: 1, editable: true},
-  rowData: rowData,
-  rowSelection: 'multiple',
-  animateRows: true,
-};
-
-// Create a row to add in the table
-function createNewRowData() {
-  var newData = {
-    asset: 'asset',
-    category: 'category',
-    signal_ticker: 'signal ticker ',
-    future_ticker: 'future ticker',
-    costs: 'costs',
-    leverage: 'leverage'
-  };
-  return newData;
-}
-
-// Clear the data from the table (clear all)
-function clearData() {
-  gridOptions.api.setRowData([]);
-}
-
-// Add a new line in the table => let the user to enter new asset
-function onAddRow() {
-    var newItem = createNewRowData();
-    var res = gridOptions.api.updateRowData({add: [newItem]});
-    printResult(res);
-}
-
-// Remove a row from the table
-function onRemoveSelected() {
-  var selectedData = gridOptions.api.getSelectedRows();
-  var res = gridOptions.api.applyTransaction({ remove: selectedData });
-  printResult(res);
-}
-
-// Get the data from assets table
-//function getDataFromTable(){
-////    var asset = $("#input_asset_from_times").val();
-////    var category = $("#input_category_from_times").val();
-////    var signalTicker = $("#input_signal_ticker_from_times").val();
-////    var futureTicker = $("#input_future_ticker_from_times").val();
-////    var costs = parseFloat($("#input_costs_from_times").val());
-////    var leverage = parseFloat($("#input_leverage_from_times").val());
-////    var versionName = $("#input_version_name_strategy").val();
-////
-////    var json_data = JSON.stringify({"input_asset": asset,
-////                                    "input_category": category,
-////                                    "input_signal_ticker": signalTicker,
-////                                    "input_future_ticker": futureTicker,
-////                                    "input_costs": costs,
-////                                    "input_leverage": leverage,
-////                                    "input_version_name_strategy": versionName});
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-////    return json_data;
-//}
-
-
-
-
-
-$(function()
+function getDataFromTable()
 {
-   $("#contact-form-button-times").click(function()
-   {
+      var assets = [];
+      var categories = [];
+      var signalTickers = [];
+      var futureTickers = [];
+      var costs = [];
+      var leverages = [];
+
       $("#faqs").find('tr').each(function(){
-
-         var asset_title = $(this).find("th:eq(0)").val();
-
 
          var asset = $(this).find("td:eq(0) input[type='text']").val();
          var category = $(this).find("td:eq(1) input[type='text']").val();
          var signalTicker = $(this).find("td:eq(2) input[type='text']").val();
          var futureTicker = $(this).find("td:eq(3) input[type='text']").val();
-         var costs = $(this).find("td:eq(4) input[type='text']").val();
+         var cost = $(this).find("td:eq(4) input[type='text']").val();
          var leverage = $(this).find("td:eq(5) input[type='text']").val();
 
-        console.log(asset_title);
-         console.log(asset);
-         console.log(category);
-        console.log(signalTicker);
-        console.log(futureTicker);
-        console.log(costs);
-        console.log(leverage);
+        assets.push(asset);
+        categories.push(category);
+        signalTickers.push(signalTicker);
+        futureTickers.push(futureTicker);
+        costs.push(parseFloat(cost));
+        leverages.push(parseFloat(leverage));
    });
- });
-});
+
+    assets.shift();
+    categories.shift();
+    signalTickers.shift();
+    futureTickers.shift();
+    costs.shift();
+    leverages.shift();
+
+    var versionName = $("#input_version_name_strategy").val();
+
+    var jsonData = JSON.stringify({"input_asset": assets,
+                                   "input_category": categories,
+                                   "input_signal_ticker": signalTickers,
+                                   "input_future_ticker": futureTickers,
+                                   "input_costs": costs,
+                                   "input_leverage": leverages,
+                                   "input_version_name_strategy": versionName});
+
+    return jsonData;
+
+};
 
 
 
@@ -214,36 +131,28 @@ function selectDateToCalendar() {
 
 
 
-//$(function(){
-//	$('#contact-form-button-times').click(function(){
-//
-//	    var json_data = getDataFromTable();
-//        var form_data = $('form').serialize();
-//        var check = checkReceivedDataTimes();
-//
-//        if (check != 'error'){
-//            $.ajax({
-//                url: "received_data_times_form",
-//                data: {form_data: form_data, json_data:json_data},
-//                type: 'POST',
-//                success: function(response){
-//                    console.log(response);
-//                    alert('The strategy has been run successfully!');
-//                    window.location.href = "times_dashboard";
-//                },
-//                error: function(error){
-//                    console.log(error);
-//                }
-//            });
-//
-//		}
-//	});
-//});
+$(function(){
+	$('#contact-form-button-times').click(function(){
+	    var jsonData = getDataFromTable();
+	    console.log(jsonData);
+        var form_data = $('form').serialize();
+        var check = checkReceivedDataTimes();
 
+        if (check != 'error'){
+            $.ajax({
+                url: "received_data_times_form",
+                data: {form_data: form_data, json_data:jsonData},
+                type: 'POST',
+                success: function(response){
+                    console.log(response);
+                    alert('The strategy has been run successfully!');
+                    window.location.href = "times_dashboard";
+                },
+                error: function(error){
+                    console.log(error);
+                }
+            });
 
-// wait for the document to be loaded, otherwise
-// ag-Grid will not find the div in the document.
-//document.addEventListener('DOMContentLoaded', function () {
-//  var eGridDiv = document.querySelector('#myGrid');
-//  new agGrid.Grid(eGridDiv, gridOptions);
-//});
+		}
+	});
+});
