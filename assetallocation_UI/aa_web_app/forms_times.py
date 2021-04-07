80cce6109bd78f697e186fbecdba3c72707192b7
Anais Jeremie <anais.jeremie@lgim.com>
2021-04-07 09:30:57 +0100

    Removed unused lines in form_times

--- assetallocation_UI/aa_web_app/forms_times.py
+++ assetallocation_UI/aa_web_app/forms_times.py
@@ -1,7 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, FieldList, FormField
-from wtforms.validators import DataRequired
-
+from wtforms import StringField, SubmitField
 from assetallocation_arp.common_libraries.dal_enums.strategy import Name
 from assetallocation_UI.aa_web_app.service.strategy import get_strategy_versions
 from assetallocation_UI.aa_web_app.service.fund import get_fund_names
@@ -25,15 +23,3 @@ class InputsTimesModel(FlaskForm):
     start_date_times_inputs = StringField('Start Date')
     end_date_times_inputs = StringField('End Date')
     submit_ok_positions = SubmitField('ok')
-
-    # # Inputs
-    # strategy_weight = StringField(u'Strategy Weight', [DataRequired(message="The strategy weight is required")])
-    # time_lag = StringField(u'Time Lag', validators=[DataRequired(message="The time lag is required")])
-    #
-    # volatility_window = StringField(u'Volatility Window', validators=[DataRequired(message="The volatility window is required")])
-    # sig1_short = StringField(u'Sigma1 short', validators=[DataRequired(message="The Sigma1 short is required")])
-    # sig1_long = StringField(u'Sigma1 long', validators=[DataRequired(message="The Sigma1 long is required")])
-    # sig2_short = StringField(u'Sigma2 short', validators=[DataRequired(message="The Sigma2 short is required")])
-    # sig2_long = StringField(u'Sigma2 long', validators=[DataRequired(message="The Sigma2 long is required")])
-    # sig3_short = StringField(u'Sigma3 short', validators=[DataRequired(message="The Sigma3 short is required")])
-    # sig3_long = StringField(u'Sigma3 long', validators=[DataRequired(message="The Sigma3 long is required")])
