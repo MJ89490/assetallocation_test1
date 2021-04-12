ec5ae1174114e567cea9ce2fa62811a92228e386
JS89275 <jessica.smart@lgim.com>
2021-04-12 15:20:02 +0100

    Update insert_strategy and insert_fund_strategy_results to read from new auth.user table

--- assetallocation_arp/data_etl/dal/database_scripts/functions/arp/insert_strategy.sql
+++ assetallocation_arp/data_etl/dal/database_scripts/functions/arp/insert_strategy.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE FUNCTION arp.insert_strategy(
   name varchar,
+  business_date_from date,
   description varchar,
   app_user_id varchar,
 	execution_state_id int,
@@ -9,13 +10,18 @@ language plpgsql
 as
 $$
 BEGIN
-INSERT INTO arp.strategy (name, description, app_user_id, execution_state_id)
-VALUES (
-  name,
-  description,
-  app_user_id,
-  execution_state_id
-)
+INSERT INTO arp.strategy(name, business_date_from, description, user_id, execution_state_id)
+SELECT
+  insert_strategy.name,
+  insert_strategy.business_date_from,
+  insert_strategy.description,
+  au.id,
+  insert_strategy.execution_state_id
+FROM
+  auth.user au
+WHERE
+  au.windows_username = app_user_id
+  OR au.domino_username = app_user_id
 RETURNING arp.strategy.id into strategy_id;
 return;
 END
