46d7d241bdd452e70a419253e15bf6557abcbedb
Anais Jeremie <anais.jeremie@lgim.com>
2021-04-28 10:52:16 +0100

    Testing app on Domino

--- assetallocation_UI/aa_web_app/routes.py
+++ assetallocation_UI/aa_web_app/routes.py
@@ -103,7 +103,7 @@ def receive_data_from_times_strategy_form():
     form_data.append('input_version_name=' + json_data['input_version_name_strategy'])
     obj_received_data_times.version_description = json_data["input_version_name_strategy"]
     obj_received_data_times.received_data_times(form_data)
-    obj_received_data_times.call_run_times(json_data)
+    # obj_received_data_times.call_run_times(json_data)
 
     return json.dumps({'status': 'OK'})
 
